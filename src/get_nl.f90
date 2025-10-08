bfc4990dcdf512cbab893525aff2cdfb9ee439dc
Kushal <kush.n00@gmail.com>
2025-10-08 13:28:41 +0200

    Correction in the volumetric heating get_nl.f90 chnage in equation (energy), Pr replace with 1

--- src/get_nl.f90
+++ src/get_nl.f90
@@ -462,7 +462,7 @@ contains
          end if
 
          if ( l_vol_heat ) then
-            this%heatTerms(:,nPhi)= opr
+            this%heatTerms(:,nPhi)= one
          end if
       end do
       !$omp end parallel
