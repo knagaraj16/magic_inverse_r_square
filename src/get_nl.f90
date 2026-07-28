50eb762a19e54b37e641cca4714af96a9b1ed622
Kushal Nagaraj <nagarkus@b-tu.de>
2026-07-28 12:12:23 +0200

    Updating energy equation from cos to sin funtion
    changed the energy equation from cosine function to sine function as theta is 0 at top and 180 at the bottom

--- src/get_nl.f90
+++ src/get_nl.f90
@@ -462,7 +462,7 @@ contains
          end if
 
          if ( l_vol_heat ) then
-            this%heatTerms(:,nPhi)= cosTheta(:)
+            this%heatTerms(:,nPhi)= sinTheta(:)
          end if
       end do
       !$omp end parallel
